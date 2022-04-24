3d1a07a6f58d4a571dd0bc0608625502ef8863a4
Aman10-02 <a.agarwala@iitg.ac.in>
2022-04-24 22:40:28 +0530

    changes to deploy

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,47 +11,47 @@ function Home() {
             topbtntext= "custom order"
             botbtntext= "existing inventory"
             backgroundimg= "/images/model-3.jpg"
-            arrow="/images/down-arrow.svg"
+            arrow="images/down-arrow.svg"
             />
             <Section 
                 title= "Model Y"
                 description="Order Online For Touchless Delivery"
                 topbtntext= "custom order"
                 botbtntext= "existing inventory"
-                backgroundimg= "/images/model-y.jpg"
+                backgroundimg= "images/model-y.jpg"
             />
         <Section 
             title= "Model S"
             description="Order Online For Touchless Delivery"
             topbtntext= "custom order"
             botbtntext= "existing inventory"
-            backgroundimg= "/images/model-s.jpg"
+            backgroundimg= "images/model-s.jpg"
             />
         <Section 
             title= "Model X"
             description="Order Online For Touchless Delivery"
             topbtntext= "custom order"
             botbtntext= "existing inventory"
-            backgroundimg= "/images/model-x.jpg"
+            backgroundimg= "images/model-x.jpg"
             />
         <Section 
             title= "Solar Panels"
             description="Lowest Cost Solar Panels in America"
             topbtntext= "order now"
             botbtntext= "Learn More"
-            backgroundimg= "/images/solar-panel.jpg"
+            backgroundimg= "images/solar-panel.jpg"
             />
         <Section 
             title= "Solar Roof"
             description="Produce Clean Energy From Your Roof"
             topbtntext= "order now"
             botbtntext= "Learn More"
-            backgroundimg= "/images/solar-roof.jpg"
+            backgroundimg= "images/solar-roof.jpg"
             />
         <Section 
             title= "Accessories"
             topbtntext= "shop now"
-            backgroundimg= "/images/accessories.jpg"
+            backgroundimg= "images/accessories.jpg"
         />
         
     </Container>
